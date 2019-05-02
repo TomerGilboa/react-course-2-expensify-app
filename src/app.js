e3da206e87bc941e34cffdbc3f12c2e407d47567
Tomer Gilboa <tomer02g@gmail.com>
2019-05-02 15:23:42 +0200

    Removed unused imports

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense, editExpense, removeExpense } from './actions/expenses';
-import  { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 
 import './styles/styles.scss';
 
